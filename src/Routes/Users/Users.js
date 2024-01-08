286151a90130ee0e6a8764722bb4baa22e832736
buihiep <bhoanghiep123456@gmail.com>
2024-01-08 18:14:41 +0700

    add form modal

--- src/Routes/Users/Users.js
+++ src/Routes/Users/Users.js
@@ -1,9 +1,8 @@
 // @ts-nocheck
-
 import React, { useEffect, useState } from 'react'
 import { userRef } from '../../config/firebase_config'
 import {getDocs, query,limit} from "firebase/firestore"; 
-import { Table} from 'react-bootstrap';
+import { Table, Button,Modal,Form} from 'react-bootstrap';
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { FcPrevious , FcNext} from "react-icons/fc";
 import ReactPaginate from 'react-paginate';
@@ -13,8 +12,11 @@ import './users.css'
 function Users() {
     const [userList, setUserList]=useState([]);
     const [currentPage, setCurrentPage] = useState(0);
-    const [totalPages, setTotalPages] = useState(10);
+    const [totalPages, setTotalPages] = useState(0);
     const rowPerPage=10
+
+    const [show, setShow]=useState(false)
+    const[dataModal, setDataModal]=useState({})
     
     useEffect(()=>{
         const loadData= async ()=>{
@@ -34,17 +36,13 @@ function Users() {
     const rowEnd=rowStart + rowPerPage
     const subList=userList.slice(rowStart,rowEnd);
     console.log(`row start ${rowStart}, ${rowEnd}: `+subList)
-    const handlePageChange= (slectedPage)=>{
-      console.log('selected page: '+ slectedPage)
-      setCurrentPage(slectedPage)
-    }
 
   return (
     <>
-      <button type="button" class="btn btn-primary btn-modal " data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+      
+      <Button className='btn btn-primary btn-modal' onClick={() => setShow(true)}>
         Add user
-      </button>
-
+      </Button>
       <Table striped bordered hover className='my-2' >
         <thead>
           <tr className='text-center'>
@@ -80,35 +78,104 @@ function Users() {
             })
           }
         </tbody>
-    </Table>
-    <ReactPaginate
-        containerClassName={"pagination"}
-        pageClassName={"page-item"}
-        activeClassName={"active-page"}
-        pageCount={totalPages}
-        onPageChange={(event)=>{setCurrentPage(event.selected)}}
-        forcePage={currentPage}
-        nextLabel={<FcNext />}
-        previousLabel={<FcPrevious />}
-        pageRangeDisplayed={3}
-    />
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
+      </Table>
+
+      <ReactPaginate
+          containerClassName={"pagination"}
+          pageClassName={"page-item"}
+          activeClassName={"active-page"}
+          pageCount={totalPages}
+          onPageChange={(event)=>{setCurrentPage(event.selected)}}
+          forcePage={currentPage}
+          nextLabel={<FcNext />}
+          previousLabel={<FcPrevious />}
+          pageRangeDisplayed={3}
+      />
+      <Modal
+        scrollable
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Add and update User
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" >
+              <Form.Label>Full name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="ng van A"
+                name='fullname'
+                
+                
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                name='email'
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter your pass"
+                name='pass'
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+              <Form.Label>Phone</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0123456789"
+                name='phone'
+              />
+            </Form.Group>
+          
+            <Form.Group className="mb-3" >
+              <Form.Label>address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="1 Dai Co Viet"
+                name='address'
+              />
+            </Form.Group>
+
+            <Form.Select  name='status' aria-label="Default select example">
+              <option>Choose Status </option>
+              <option value="1">Active</option>
+              <option value="2">Inactive</option>
+            </Form.Select>
+
+            <Form.Group className="mb-3" >
+              <Form.Label>Avatar</Form.Label>
+              <Form.Control
+                type="file"
+                name='avatar'
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShow(false)}>
+            Close
+          </Button>
+          <Button type='submit' onClick={() => setShow(false)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
 
   )
